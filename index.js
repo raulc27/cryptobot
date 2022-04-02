19dfffc5551a047f717fbcb0cb735afa1bc2b03c
Raul Castro <raul.de.almeida.neto@accenture.com>
2022-04-02 15:25:13 -0300

    consertando erro

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const GDAX = require("gdax");
-const publicClient = new GDAX.PublicClient();
-const express = requires('express');
+
+const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const callback = (error, response, data) => {
-    if(error)
-        return console.dir(error);
-    return data;
-}
+
 
 app.use(cors());
 app.use(express.json());
@@ -17,4 +13,3 @@ let PORT = process.env.PORT || 3337;
 
 app.listen(port);
 
-publicClient.getProducts(callback);
