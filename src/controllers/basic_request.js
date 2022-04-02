6a74d6d659345905e7d48a4e536b1d7a0ad1f01e
Raul Castro <raul.de.almeida.neto@accenture.com>
2022-04-02 15:41:37 -0300

    ultimas alteracoes

--- src/controllers/basic_request.js
+++ src/controllers/basic_request.js
@@ -3,12 +3,16 @@ const GDAX = require("gdax");
 const publicClient = new GDAX.PublicClient();
 
 exports.get = async(request,response,next)=>{
-
+   try{
     const callback = (error, response, data) => {
         if(error)
             return console.dir(error);
         return data;
     }
    return response.json(publicClient.getProducts(callback));
+   }
+   catch(_err){
+       console.log(_err);
+   }
 
 }
