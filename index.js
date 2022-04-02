a3630da9638e7843406031022a1b89d078c9b1fe
Raul Castro <raul.de.almeida.neto@accenture.com>
2022-04-02 15:29:16 -0300

    esqueci de routes... consertado

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const GDAX = require("gdax");
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const routes = require('./src/routes');
 
 
 
 app.use(cors());
 app.use(express.json());
-
+app.use(routes);
 let PORT = process.env.PORT || 3337;
 
 app.listen(PORT);
